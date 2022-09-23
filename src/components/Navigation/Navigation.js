76f97674ceafe0d0a9492bb01126ba06d2fb399f
Thao My <mylttse161097@fpt.edu.vn>
2022-09-23 18:45:37 +0700

    List of news page

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import navigation from "../Navigation/navigation.css"
+export default class Navigation extends Component {
+  render() {
+    return (
+      <div className="navbar ">
+        <nav class="nav grid-container">
+          <div className="nav-logo">
+          <a href="#home"> MEDICHOR</a>
+          </div>
+          {/* <p href="#logo">MEDICHOR</p> */}
+          <div className="nav-links">
+            <a href="#home">Trang Chủ</a>
+            <a href="#campaign">Chiến Dịch</a>
+            <a href="#news">News</a>
+            <a href="#achievement">Thành Tích</a>
+          </div>
+
+          <div className="nav-logs">
+            <a href="#signin" class="split">
+              Đăng Nhập
+            </a>
+            <a href="#sign" class="split">
+              Đăng Ký
+            </a>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
---
+++ src/components/Pagination.js
@@ -0,0 +1,8 @@
+import { Pagination } from 'antd';
+import PaginationStyle from './PaginationStyle.css';
+
+import React, { Component } from 'react'
+
+const App = () => <Pagination defaultCurrent={1} total={50} />;
+
+export default App;
