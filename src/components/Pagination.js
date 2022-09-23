46bcce9046451260266fe6b2004edcbbb7db19f0
Thao My <mylttse161097@fpt.edu.vn>
2022-09-23 18:33:58 +0700

    Add scrolling text

--- src/components/Pagination.js
+++
@@ -1,8 +0,0 @@
-import { Pagination } from 'antd';
-import PaginationStyle from './PaginationStyle.css';
-
-import React, { Component } from 'react'
-
-const App = () => <Pagination defaultCurrent={1} total={50} />;
-
-export default App;
