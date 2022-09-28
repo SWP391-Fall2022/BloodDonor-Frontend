2fcbcddd49e6cb67fae78cb9650b0be3d4715643
TMQuan202 <quantm.lqd@gmail.com>
2022-09-28 22:58:08 +0700

    Add recaptcha and env
    - Recaptcha added to login and change password pages
    - Create and hide env for google recaptcha and login keys for security

--- src/routes/changePass.js
+++ src/routes/changePass.js
@@ -1,37 +1,93 @@
 import styles from '../styles/login.module.css';
 import '../index.css';
+import { Component } from "react";
 import { Button, Form, Input } from 'antd';
+import ReCAPTCHA from "react-google-recaptcha";
 
-const newPass = () => {
+class newPass extends Component {
 
     //Need a system to check login before come to this page
     //Need a system to check old Password before change the new one
-    //Need a system to check 2 passwords here are the same
+    constructor(props) {
+        super(props);
+        this.state = {
+            oldPass: '',
+            newPass: '',
+            newPassDupCheck: '',
+            message: '',
+            recaptchaCheck: false,
+        }
+    }
 
-    return (
-        <div className={styles.mainBackground}>
-            <div className={`${styles.container} shadowDP02-border10`}>
-                <h1 className={`${styles.title}`}>ĐỔI MẬT KHẨU</h1>
-                <Form layout="vertical">
-                    <Form.Item className={styles.formLabel} label="Nhập mật khẩu cũ" name="oldPass" rules={[{ required: true, message: 'Vui lòng nhập vào đây' }]}>
-                        <Input.Password placeholder="Nhập mật khẩu mới" />
-                    </Form.Item>
-                    <Form.Item className={styles.formLabel} label="Nhập mật khẩu mới" name="newPass" rules={[{ required: true, message: 'Vui lòng nhập vào đây' }]}>
-                        <Input.Password placeholder="Nhập mật khẩu mới" />
-                    </Form.Item>
-                    <Form.Item className={styles.formLabel} label="Nhập lại mật khẩu mới" name="newPassDupCheck" rules={[{ required: true, message: 'Vui lòng nhập vào đây' }]}>
-                        <Input.Password placeholder="Nhập lại mật khẩu mới" />
-                    </Form.Item>
-                    {/* recaptcha here */}
-                    <Form.Item>
-                        <Button className={`${styles.btn}`} type="primary" htmlType="submit" size="large">
-                            Đổi mật khẩu
-                        </Button>
-                    </Form.Item>
-                </Form>
+    handleOnChangeOldPassword = (event) => {
+        this.setState({
+            oldPass: event.target.value
+        })
+    }
+
+    handleOnChangeNewPassword = (event) => {
+        this.setState({
+            newPass: event.target.value
+        })
+    }
+
+    handleOnChangeNewPasswordDup = (event) => {
+        this.setState({
+            newPassDupCheck: event.target.value
+        })
+    }
+
+    onChangeRecaptcha = (value) => {
+        if (value !== null) {
+            this.setState({ recaptchaCheck: true })
+        }
+    }
+
+    handleChangePass = async () => {
+        if (!this.state.recaptchaCheck) {
+            this.setState({ message: "Vui lòng xác nhận reCaptcha" })
+        } else if (this.state.newPass !== this.state.newPassDupCheck) {
+            this.setState({ message: "Hai mật khẩu mới không trùng khớp" })
+        } else {
+            this.setState({ message: 'Đổi mật khẩu thành công (test mess)'})
+        }
+    }
+
+    render() {
+        return (
+            <div className={styles.mainBackground}>
+                <div className={`${styles.container} shadowDP02-border10`}>
+                    <h1 className={`${styles.title}`}>ĐỔI MẬT KHẨU</h1>                    
+                    <div style={{ color: 'red', textAlign: 'center', fontWeight: 'bold', marginBottom: '1rem'}}>
+                        {this.state.message}
+                    </div>
+                    <Form layout="vertical">
+                        <Form.Item className={styles.formLabel} label="Nhập mật khẩu cũ" name="oldPass" rules={[{ required: true, message: 'Vui lòng nhập vào đây' }]}>
+                            <Input.Password placeholder="Nhập mật khẩu mới" onChange={this.handleOnChangeOldPassword}/>
+                        </Form.Item>
+                        <Form.Item className={styles.formLabel} label="Nhập mật khẩu mới" name="newPass" rules={[{ required: true, message: 'Vui lòng nhập vào đây' }]}>
+                            <Input.Password placeholder="Nhập mật khẩu mới" onChange={this.handleOnChangeNewPassword}/>
+                        </Form.Item>
+                        <Form.Item className={styles.formLabel} label="Nhập lại mật khẩu mới" name="newPassDupCheck" rules={[{ required: true, message: 'Vui lòng nhập vào đây' }]}>
+                            <Input.Password placeholder="Nhập lại mật khẩu mới" onChange={this.handleOnChangeNewPasswordDup}/>
+                        </Form.Item>
+                        <Form.Item className={`${styles.formLabel} ${styles.recapcha}`}>
+                            <ReCAPTCHA
+                                style={{ margin: "0 25px" }}
+                                sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
+                                onChange={this.onChangeRecaptcha}
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button className={`${styles.btn}`} type="primary" htmlType="submit" size="large" onClick={this.handleChangePass}>
+                                Đổi mật khẩu
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </div>
             </div>
-        </div>
-    )
-};
+        )
+    }
+}
 
 export default newPass;
--- src/routes/login.js
+++ src/routes/login.js
@@ -1,8 +1,10 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import styles from '../styles/login.module.css';
 import '../index.css';
 import { Button, Form, Input } from 'antd';
 import { Component } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 
 class Login extends Component {
     constructor(props) {
@@ -11,6 +13,13 @@ class Login extends Component {
             username: '',
             password: '',
             message: '',
+            recaptchaCheck: false,
+        }
+    }
+
+    onChangeRecaptcha = (value) => {
+        if (value !== null) {
+            this.setState({ recaptchaCheck: true })
         }
     }
 
@@ -27,24 +36,29 @@ class Login extends Component {
     }
 
     handleLogin = async () => {
-        let json = {
-            method: 'POST',
-            body: JSON.stringify({ "username": this.state.username, "password": this.state.password }),
-            headers: new Headers({
-                'Content-Type': 'application/json; charset=UTF-8'
-            })
-        }
-        const response = await fetch("http://localhost:8080/v1/login", json)
-            .then((res) => res.json())
-            .catch((error) => { console.log(error) })
-        if (response.JSON.success) {
-            this.setState({
-                message: 'Login successful!'
-            })
+        if (!this.state.recaptchaCheck) {
+            this.setState({ message: "Please check reCaptcha" })
         } else {
-            this.setState({
-                message: 'Wrong email or password'
-            })
+            this.setState({ message: '' })
+            let json = {
+                method: 'POST',
+                body: JSON.stringify({ "username": this.state.username, "password": this.state.password }),
+                headers: new Headers({
+                    'Content-Type': 'application/json; charset=UTF-8'
+                })
+            }
+            const response = await fetch("http://localhost:8080/v1/login", json)
+                .then((res) => res.json())
+                .catch((error) => { console.log(error) })
+            if (response.JSON.success) {
+                this.setState({
+                    message: 'Login successful!'
+                })
+            } else {
+                this.setState({
+                    message: 'Wrong email or password'
+                })
+            }
         }
     }
 
@@ -60,13 +74,20 @@ class Login extends Component {
                         <Form.Item className={styles.formLabel} label="Mật khẩu" name="Password" rules={[{ required: true, message: 'Vui lòng nhập mật khẩu' }]}>
                             <Input.Password placeholder="Nhập mật khẩu" value={this.state.password} onChange={this.handleOnChangePassword} />
                         </Form.Item>
-                        <Form.Item>
+                        <Form.Item className={styles.formLabel}>
                             <Button className={`${styles.btn}`} type="primary" htmlType="submit" size="large" onClick={this.handleLogin}>
                                 Đăng nhập
                             </Button>
                         </Form.Item>
+                        <Form.Item className={`${styles.formLabel} ${styles.recapcha}`}>
+                            <ReCAPTCHA
+                                style={{ margin: "0 25px" }}
+                                sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
+                                onChange={this.onChangeRecaptcha}
+                            />
+                        </Form.Item>
                     </Form>
-                    <div style={{color: 'red'}}>
+                    <div style={{ color: 'red', textAlign: 'center', fontWeight: 'bold', marginBottom: '1rem' }}>
                         {this.state.message}
                     </div>
                     <div className={`${styles.underInfo}`}>
