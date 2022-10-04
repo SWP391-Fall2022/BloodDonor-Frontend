41483f05e32b92491463fe160f03e8a487d36be9
TMQuan202 <quantm.lqd@gmail.com>
2022-10-04 13:46:36 +0700

    Add Page Footer Bottom part only

---
+++ src/components/Footer/PageFooterBottom.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PageFooterStyle from './PageFooterStyle.css';
+
+export const PageFooter = () => {
+  return (
+    <footer className='footer-container'>
+      <div className='foot-container-bot'>Copyright © 2022 Trang thông tin và đăng ký hiến máu Medichor</div>
+    </footer>
+  )
+}
