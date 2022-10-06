e6eaa3b87c6f2769f16c9d0be9ddd699e39ae2d2
WhiteSpace <phamkhanhtn177@gmail.com>
2022-10-06 21:51:58 +0700

    ADD - Create homepage and Q&A

---
+++ src/routes/Homepage/Campaign/Campaign.js
@@ -0,0 +1,250 @@
+import "./campaign.css";
+import logo_1 from "../../../assets/organization-logo-1.png";
+import logo_2 from "../../../assets/organization-logo-2.png";
+import React from "react";
+import { Carousel } from "antd";
+const Campaign = () => {
+  return (
+    <section className="campaign">
+      <h2>CÁC CHIẾN DỊCH ĐANG DIỄN RA</h2>
+      <Carousel autoplay draggable={true} touchThreshold={30} dots={false}>
+        {/* SLIDE 2 */}
+        <div>
+          <div className="container-campaign">
+            {/* ===1=== */}
+            <div className="campaign-card">
+              <div className="campaign-header">
+                <a className="campaign-organization-name" href="#organization">
+                  BV Truyền máu huyết học
+                </a>
+              </div>
+              <div className="campaign-img-container">
+                <img src={logo_1} alt="logo_1"></img>
+              </div>
+              <div className="campaign-card-content">
+                <a href="#name" className="campaign-name">
+                  Chiến dịch 104
+                </a>
+                <a href="#address" className="campaign-address">
+                  118 Hồng Bàng, Phường 12, Quận 5, TP.HCM
+                </a>
+                <div className="campaign-time">
+                  Từ 15/09/2022 đến 20/10/2022{" "}
+                </div>
+              </div>
+              <div className="campaign-card-footer">
+                <div className="campaign-react">Tym</div>
+                <a className="campaign-report" href="#report">
+                  Báo cáo
+                </a>
+              </div>
+            </div>
+            {/* ===2=== */}
+            <div className="campaign-card">
+              <div className="campaign-header">
+                <a className="campaign-organization-name" href="#organization">
+                  BV Truyền máu huyết học
+                </a>
+              </div>
+              <div className="campaign-img-container">
+                <img src={logo_1} alt="logo_1"></img>
+              </div>
+              <div className="campaign-card-content">
+                <a href="#name" className="campaign-name">
+                  Chiến dịch 104
+                </a>
+                <a href="#address" className="campaign-address">
+                  118 Hồng Bàng, Phường 12, Quận 5, TP.HCM
+                </a>
+                <div className="campaign-time">
+                  Từ 15/09/2022 đến 20/10/2022{" "}
+                </div>
+              </div>
+              <div className="campaign-card-footer">
+                <div className="campaign-react">Tym</div>
+                <a className="campaign-report" href="#report">
+                  Báo cáo
+                </a>
+              </div>
+            </div>
+            {/* ===3=== */}
+            <div className="campaign-card">
+              <div className="campaign-header">
+                <a className="campaign-organization-name" href="#organization">
+                  BV Truyền máu huyết học
+                </a>
+              </div>
+              <div className="campaign-img-container">
+                <img src={logo_1} alt="logo_1"></img>
+              </div>
+              <div className="campaign-card-content">
+                <a href="#name" className="campaign-name">
+                  Chiến dịch 104
+                </a>
+                <a href="#address" className="campaign-address">
+                  118 Hồng Bàng, Phường 12, Quận 5, TP.HCM
+                </a>
+                <div className="campaign-time">
+                  Từ 15/09/2022 đến 20/10/2022{" "}
+                </div>
+              </div>
+              <div className="campaign-card-footer">
+                <div className="campaign-react">Tym</div>
+                <a className="campaign-report" href="#report">
+                  Báo cáo
+                </a>
+              </div>
+            </div>
+            {/* ===4=== */}
+            <div className="campaign-card">
+              <div className="campaign-header">
+                <a className="campaign-organization-name" href="#organization">
+                  BV Truyền máu huyết học
+                </a>
+              </div>
+              <div className="campaign-img-container">
+                <img src={logo_1} alt="logo_1"></img>
+              </div>
+              <div className="campaign-card-content">
+                <a href="#name" className="campaign-name">
+                  Chiến dịch 104
+                </a>
+                <a href="#address" className="campaign-address">
+                  118 Hồng Bàng, Phường 12, Quận 5, TP.HCM
+                </a>
+                <div className="campaign-time">
+                  Từ 15/09/2022 đến 20/10/2022{" "}
+                </div>
+              </div>
+              <div className="campaign-card-footer">
+                <div className="campaign-react">Tym</div>
+                <a className="campaign-report" href="#report">
+                  Báo cáo
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* SLIDE 2 */}
+        <div>
+          <div className="container-campaign">
+            {/* ===1=== */}
+            <div className="campaign-card">
+              <div className="campaign-header">
+                <a className="campaign-organization-name" href="#organization">
+                  BV Truyền máu huyết học
+                </a>
+              </div>
+              <div className="campaign-img-container">
+                <img src={logo_2} alt="logo_1"></img>
+              </div>
+              <div className="campaign-card-content">
+                <a href="#name" className="campaign-name">
+                  Chiến dịch 104
+                </a>
+                <a href="#address" className="campaign-address">
+                  118 Hồng Bàng, Phường 12, Quận 5, TP.HCM
+                </a>
+                <div className="campaign-time">
+                  Từ 15/09/2022 đến 20/10/2022{" "}
+                </div>
+              </div>
+              <div className="campaign-card-footer">
+                <div className="campaign-react">Tym</div>
+                <a className="campaign-report" href="#report">
+                  Báo cáo
+                </a>
+              </div>
+            </div>
+            {/* ===2=== */}
+            <div className="campaign-card">
+              <div className="campaign-header">
+                <a className="campaign-organization-name" href="#organization">
+                  BV Truyền máu huyết học
+                </a>
+              </div>
+              <div className="campaign-img-container">
+                <img src={logo_2} alt="logo_1"></img>
+              </div>
+              <div className="campaign-card-content">
+                <a href="#name" className="campaign-name">
+                  Chiến dịch 104
+                </a>
+                <a href="#address" className="campaign-address">
+                  118 Hồng Bàng, Phường 12, Quận 5, TP.HCM
+                </a>
+                <div className="campaign-time">
+                  Từ 15/09/2022 đến 20/10/2022{" "}
+                </div>
+              </div>
+              <div className="campaign-card-footer">
+                <div className="campaign-react">Tym</div>
+                <a className="campaign-report" href="#report">
+                  Báo cáo
+                </a>
+              </div>
+            </div>
+            {/* ===3=== */}
+            <div className="campaign-card">
+              <div className="campaign-header">
+                <a className="campaign-organization-name" href="#organization">
+                  BV Truyền máu huyết học
+                </a>
+              </div>
+              <div className="campaign-img-container">
+                <img src={logo_2} alt="logo_1"></img>
+              </div>
+              <div className="campaign-card-content">
+                <a href="#name" className="campaign-name">
+                  Chiến dịch 104
+                </a>
+                <a href="#address" className="campaign-address">
+                  118 Hồng Bàng, Phường 12, Quận 5, TP.HCM
+                </a>
+                <div className="campaign-time">
+                  Từ 15/09/2022 đến 20/10/2022{" "}
+                </div>
+              </div>
+              <div className="campaign-card-footer">
+                <div className="campaign-react">Tym</div>
+                <a className="campaign-report" href="#report">
+                  Báo cáo
+                </a>
+              </div>
+            </div>
+            {/* ===4=== */}
+            <div className="campaign-card">
+              <div className="campaign-header">
+                <a className="campaign-organization-name" href="#organization">
+                  BV Truyền máu huyết học
+                </a>
+              </div>
+              <div className="campaign-img-container">
+                <img src={logo_2} alt="logo_1"></img>
+              </div>
+              <div className="campaign-card-content">
+                <a href="#name" className="campaign-name">
+                  Chiến dịch 104
+                </a>
+                <a href="#address" className="campaign-address">
+                  118 Hồng Bàng, Phường 12, Quận 5, TP.HCM
+                </a>
+                <div className="campaign-time">
+                  Từ 15/09/2022 đến 20/10/2022{" "}
+                </div>
+              </div>
+              <div className="campaign-card-footer">
+                <div className="campaign-react">Tym</div>
+                <a className="campaign-report" href="#report">
+                  Báo cáo
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Carousel>
+    </section>
+  );
+};
+
+export default Campaign;
---
+++ src/routes/Homepage/Homepage.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { PageFooter } from "../../components/Footer/PageFooter";
+import { Navbar } from "../../components/navbar";
+import Achievements from "./Achievements/Achievement";
+import Advice from "./Advice/Advice";
+import Benefits from "./Benefits/Benefits";
+import Campaign from "./Campaign/Campaign";
+import Cover from "./Cover/Cover";
+import News from "./News/News";
+import QAndA from "./QAndA/QAndA";
+import Standard from "./Standard/Standard";
+const Homepage = () => {
+  return (
+    <div>
+      <Navbar />
+      <div id="homepage">
+        <Cover />
+        <Benefits />
+        <Campaign />
+        <Standard/>
+        <Advice />
+        <News />
+        <Achievements />
+        <QAndA />
+      </div>
+      <PageFooter />
+    </div>
+  );
+};
+
+export default Homepage;
