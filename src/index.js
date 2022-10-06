8750c9438804182ff3c53ed2d67fde2a1ae8e223
TMQuan202 <quantm.lqd@gmail.com>
2022-10-07 00:05:58 +0700

    Redone login-register

--- src/index.js
+++ src/index.js
@@ -3,22 +3,22 @@ import './index.css';
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from './routes/Homepage/Homepage';
-// import Home from "./routes/home";
-// import Login from "./routes/login";
-// import Register from "./routes/register";
-// import RegisterDonor from "./components/Register/RegisterDonor";
-// import RegisterPlace from "./components/Register/RegisterPlace";
+import Achievements from './routes/Homepage/Achievements/Achievement';
+import Login from "./routes/login";
+import Register from "./routes/register";
+import RegisterDonor from "./components/Register/RegisterDonor";
+import RegisterPlace from "./components/Register/RegisterPlace";
 // import NewPass from "./routes/changePass";
 // import Forget from "./routes/restore";
-// import Otp from "./routes/otp";
+import Otp from "./routes/otp";
 // import Campaign from "./routes/campaign";
 // import ListNewsPage from './routes/ListNewsPage';
 // import NewsDetailPage from './routes/NewsDetailPage';
 // import Achivement from "./routes/achivement";
-// import AuthRoutes from './components/ProtectedRoute/AuthRoutes';
+import AuthRoutes from './components/ProtectedRoute/AuthRoutes';
 
 // import DonorProfile from './routes/donor-progile';
-// import NoPage from "./routes/nopage";
+import NoPage from "./routes/nopage";
 // import Info from './components/Donor-Profile/Basic Info/Info';
 // import ChangeEmail from './components/Donor-Profile/Change Email/changeEmail';
 // import ChangePassword from './components/Donor-Profile/Change Password/changePassword';
@@ -33,32 +33,31 @@ export default function App() {
       <Routes>
         <Route exact strict path="/" element={<Homepage />} />
 
-        {/* <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} >
-          <Route path="donor-volunteer" element={<RegisterDonor />} />
-          <Route path="donor-place" element={<RegisterPlace />} />
-        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/register/donor" element={<RegisterDonor />} />
+        <Route path="/register/place" element={<RegisterPlace />} />
 
         <Route path="/auth" element={<AuthRoutes />} />
-        <Route path="/new-password" element={<NewPass />} />
-        <Route path="/restore" element={<Forget />} />
+        {/* <Route path="/new-password" element={<NewPass />} /> */}
+        {/* <Route path="/restore" element={<Forget />} /> */}
         <Route path="/otp" element={<Otp />} />
 
-        <Route exact strict path="/campaign" element={<Campaign />} />
-        <Route exact strict path="/news" element={<ListNewsPage />} />
-        <Route exact strict path="/news/news-detail" element={<NewsDetailPage />} />
-        <Route exact strict path="/achivement" element={<Achivement />} />
+        {/* <Route exact strict path="/campaign" element={<Campaign />} /> */}
+        {/* <Route exact strict path="/news" element={<ListNewsPage />} /> */}
+        {/* <Route exact strict path="/news/news-detail" element={<NewsDetailPage />} /> */}
+        <Route exact strict path="/achivement" element={<Achievements />} />
 
-        <Route path="/donor" element={<DonorProfile />} >
-          <Route index element={<Info />} />
-          <Route path="changeEmail" element={<ChangeEmail />} />
-          <Route path="changePassword" element={<ChangePassword />} />
-          <Route path="changePhone" element={<ChangePhone />} />
-          <Route path="history" element={<History />} />
-          <Route path="qna" element={<QnA />} />
-          <Route path="vouchers" element={<Vouchers />} />
-        </Route>
-        <Route path="*" element={<NoPage />} /> */}
+        {/* <Route path="/donor" element={<DonorProfile />} > */}
+        {/* <Route index element={<Info />} /> */}
+        {/* <Route path="changeEmail" element={<ChangeEmail />} /> */}
+        {/* <Route path="changePassword" element={<ChangePassword />} /> */}
+        {/* <Route path="changePhone" element={<ChangePhone />} /> */}
+        {/* <Route path="history" element={<History />} /> */}
+        {/* <Route path="qna" element={<QnA />} /> */}
+        {/* <Route path="vouchers" element={<Vouchers />} /> */}
+        {/* </Route> */}
+        <Route path="*" element={<NoPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/routes/otp.js
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from 'react';
+import styles from '../components/Otp/otp.module.css';
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
+import { Button } from 'antd';
+import OtpInput from "react-otp-input";
+
+export default function Otp() {
+
+    const [otp, setOtp] = useState('')
+    const [seconds, setSeconds] = useState(60)
+    const { state } = useLocation();
+    const { otpAccess } = state;
+    const navigate = useNavigate();
+    const handleChange = (otp) => {
+        setOtp(otp)
+    }
+
+    const handleSubmit = async () => {
+        let json = {
+            method: 'PUT',
+            headers: new Headers({
+                'Content-Type': 'application/json; charset=UTF-8'
+            })
+        }
+        const response = await fetch(`http://localhost:8080/v1/register/confirmCode/${otp}`, json)
+            .then((res) => res.json())
+            .catch((error) => { console.log(error) })
+        console.log(response)
+        if (response.success) {
+            navigate('/login')
+        }
+    }
+
+    //Undone
+    const reSendOtp = async () => {
+        setSeconds(60)
+        let json = {
+            method: 'PUT',
+            headers: new Headers({
+                'Content-Type': 'application/json; charset=UTF-8'
+            })
+        }
+        const response = await fetch(`http://localhost:8080/v1/register/resendCode/1004`, json)
+            .then((res) => res.json())
+            .catch((error) => { console.log(error) })
+        console.log(response)
+    }
+
+    useEffect(() => {
+        let myInterval = setInterval(() => {
+            if (seconds > 0) {
+                setSeconds(seconds - 1);
+            }
+            if (seconds === 0) {
+                clearInterval(myInterval)
+            }
+        }, 1000)
+        return () => {
+            clearInterval(myInterval);
+        };
+    });
+
+    let button;
+    if (seconds > 0) {
+        button = <Button className={`${styles.btn}`} disabled>Gửi lại mã</Button>
+    } else {
+        button = <Button className={`${styles.btn}`} onClick={reSendOtp}>Gửi lại mã</Button>
+    }
+
+    if (otpAccess === null || !otpAccess) {
+        return <Navigate to={'/login'} replace />
+    } else
+        return (
+            <div className={styles.container}>
+                <div className={styles.content}>Mã xác nhận đã được gửi qua mail của bạn</div>
+                <div className={styles.content}>Kiểm tra mail để xác nhận thông tin</div>
+                <OtpInput className={styles.otp}
+                    numInputs={6}
+                    value={otp}
+                    onChange={handleChange}
+                    isInputNum={true}
+                    containerStyle={{ display: "flex", justifyContent: "center" }}
+                    inputStyle={{
+                        width: "60px",
+                        height: "90px",
+                        margin: "30px 15px",
+                        fontSize: "64px",
+                        borderRadius: 10,
+                        border: "3px solid"
+                    }} />
+                <div className={styles.content}>Gửi lại mã trong {seconds}s</div>
+                <div style={{ textAlign: 'center' }}>
+                    <Button className={`${styles.btn}`} onClick={handleSubmit}>Xác nhận</Button>
+                    {button}                    
+                </div>
+            </div>
+        )
+}
