23c25381a792c808ef7face8ab06008267f5dfd1
quantm.lqd@gmail.com <quantm.lqd@gmail.com>
2022-11-06 12:10:38 +0700

    Add claim voucher for donor

--- src/routes/Donor-Profile/Voucher Storage/VouchersCointainer2.js
+++ src/routes/Donor-Profile/Voucher Storage/VouchersCointainer2.js
@@ -1,9 +1,11 @@
 import styles from '../donor.module.css'
 import empty from '../../../assets/empty-list.png'
-import { Card, List } from 'antd'
+import { ExclamationCircleOutlined } from '@ant-design/icons';
+import { Card, List, Modal } from 'antd'
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { useEffect } from 'react';
+const { confirm } = Modal;
 
 export default function VouchersContainer2() {
     const [emptyList, setEmptyList] = useState(false);
@@ -41,6 +43,34 @@ export default function VouchersContainer2() {
     }, [emptyList])
 
     //Claim Voucher
+    const onClaim = (id) => {
+        confirm({
+            title: 'Dùng điểm của bạn để đổi voucher này?',
+            icon: <ExclamationCircleOutlined />,
+            okText: 'Đổi',
+            cancelText: 'Hủy',
+            onOk() {
+                onConfirm(id);
+            },
+        });
+    }
+
+    const onConfirm = async (id) => {
+        const token = JSON.parse(sessionStorage.getItem('JWT_Key'))
+        let json = {
+            method: "PUT",
+            headers: new Headers({
+                'Authorization': "Bearer " + token,
+            })
+        }
+        const response = await fetch(`${process.env.REACT_APP_BACK_END_HOST}/v1/donors/me/rewards/${id}`, json)
+            .then((res) => res.json())
+            .catch((error) => { console.log(error) })
+        // console.log(response)
+        setTimeout(() => {
+            window.location.reload(false);
+        }, 1000);
+    }
 
     if (!emptyList) {
         return (
@@ -61,7 +91,7 @@ export default function VouchersContainer2() {
                         <List.Item>
                             <Card headStyle={{ fontSize: '16px', fontWeight: '700', background: 'var(--blue-02)' }} style={{ marginBottom: '20px', boxShadow: 'var(--shadow-dp-01)' }}
                                 title={item.sponsor}
-                                extra={<Link style={{ color: 'var(--blue-01)' }}>Đổi</Link>}
+                                extra={<Link style={{ color: 'var(--blue-01)' }} onClick={() => onClaim(item.id)}>Đổi</Link>}
                             >
                                 <div className={styles.innerTitle}>{item.point} Điểm</div>
                                 <div style={{ color: 'rgba(0, 0, 0, 0.45)' }}>{item.details}</div>
