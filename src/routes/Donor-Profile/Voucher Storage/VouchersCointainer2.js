863d409fdbc8af263ff96c35585f3d31567af15f
quantm.lqd@gmail.com <quantm.lqd@gmail.com>
2022-11-06 10:07:19 +0700

    Add fetch vouchers

--- src/routes/Donor-Profile/Voucher Storage/VouchersCointainer2.js
+++ src/routes/Donor-Profile/Voucher Storage/VouchersCointainer2.js
@@ -1,61 +1,88 @@
 import styles from '../donor.module.css'
+import empty from '../../../assets/empty-list.png'
 import { Card, List } from 'antd'
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 export default function VouchersContainer2() {
-    const data = [
-        {
-            title: 'Cửa hàng tiện lợi 7-Eleven',
-            point: '100 điểm',
-        },
-        {
-            title: 'Cửa hàng tiện lợi 7-Eleven',
-            point: '60 điểm',
-        },
-        {
-            title: 'Cửa hàng tiện lợi 7-Eleven',
-            point: '20 điểm',
-        },
-        {
-            title: 'Cửa hàng tiện lợi 7-Eleven',
-            point: '200 điểm',
-        }, 
-        {
-            title: 'Cửa hàng tiện lợi 7-Twelve',
-            point: '150 điểm',
-        }, 
-        {
-            title: 'Cửa hàng tiện lợi 8-Ten',
-            point: '20 điểm',
-        },
-    ];
-    return (
-        <div className={styles.voucherContainer}>
-            <div className={styles.title}>VOUCHER BẠN CÓ THỂ NHẬN</div>
-            <List
-                grid={{
-                    gutter: 16,
-                    xs: 1,
-                    sm: 1,
-                    md: 1,
-                    lg: 1,
-                    xl: 2,
-                    xxl: 2,
-                }}
-                dataSource={data}
-                renderItem={(item) => (
-                    <List.Item>
-                        <Card headStyle={{ fontSize: '16px', fontWeight: '700', background: 'var(--blue-02)' }} style={{ marginBottom: '20px', boxShadow: 'var(--shadow-dp-01)' }}
-                            title={item.title}
-                            extra={<Link style={{ color: 'var(--blue-01)' }}>Đổi</Link>}>
-                            <div className={styles.innerTitle}>{item.point}</div>
-                            <div style={{ color: 'rgba(0, 0, 0, 0.45)' }}>Voucher giảm 5k với hóa đơn từ 20k</div>
-                            <div style={{ color: 'rgba(0, 0, 0, 0.45)' }}>Hạn sử dụng đến 30/11/2022</div>
-                        </Card>
-                    </List.Item>
-                )}
-                pagination={{ total: data.length, pageSize: '4', hideOnSinglePage: true }}
-            />
-        </div>
-    )
+    const [emptyList, setEmptyList] = useState(false);
+    const [dataSource, setDataSource] = useState([])
+
+    //Load vouchers
+    useEffect(() => {
+        async function fetchAPI() {
+            const data = [];
+            const token = JSON.parse(sessionStorage.getItem('JWT_Key'))
+            let json = {
+                headers: new Headers({
+                    'Authorization': "Bearer " + token,
+                })
+            }
+            const response = await fetch(`${process.env.REACT_APP_BACK_END_HOST}/v1/donors/me/rewards/available`, json)
+                .then((res) => res.json())
+                .catch((error) => { console.log(error) })
+            // console.log(response);
+            if (response.status === 200) {
+                response.body.forEach(e => data.push({
+                    id: e.id, sponsor: e.sponsor, point: e.level, details: e.details, expiredDate: e.expiredDate
+                }))
+            }
+            // console.log(data)
+            if (data.length === 0) {
+                setEmptyList(true)
+            } else {
+                setDataSource(data)
+                setEmptyList(false)
+            }
+        }
+
+        fetchAPI();
+    }, [emptyList])
+
+    //Claim Voucher
+
+    if (!emptyList) {
+        return (
+            <div className={styles.voucherContainer}>
+                <div className={styles.title}>VOUCHER BẠN CÓ THỂ NHẬN</div>
+                <List
+                    grid={{
+                        gutter: 16,
+                        xs: 1,
+                        sm: 1,
+                        md: 1,
+                        lg: 1,
+                        xl: 2,
+                        xxl: 2,
+                    }}
+                    dataSource={dataSource}
+                    renderItem={(item) => (
+                        <List.Item>
+                            <Card headStyle={{ fontSize: '16px', fontWeight: '700', background: 'var(--blue-02)' }} style={{ marginBottom: '20px', boxShadow: 'var(--shadow-dp-01)' }}
+                                title={item.sponsor}
+                                extra={<Link style={{ color: 'var(--blue-01)' }}>Đổi</Link>}
+                            >
+                                <div className={styles.innerTitle}>{item.point} Điểm</div>
+                                <div style={{ color: 'rgba(0, 0, 0, 0.45)' }}>{item.details}</div>
+                                <div style={{ color: 'rgba(0, 0, 0, 0.45)' }}>Hạn sử dụng đến {item.expiredDate}</div>
+                            </Card>
+                        </List.Item>
+                    )}
+                    pagination={{ total: dataSource.length, pageSize: '4', hideOnSinglePage: true }}
+                />
+            </div>
+        )
+    } else {
+        return (
+            <div className={styles.voucherContainer}>
+                <div className={styles.title}>VOUCHER BẠN CÓ THỂ NHẬN</div>
+                <div style={{ textAlign: 'center' }}>
+                    <img className={styles.img} src={empty} alt="empty" />
+                    <div><strong>Không có voucher nào bạn có thể nhận được tìm thấy</strong></div>
+                    <div><strong>Hãy tích thêm điểm thưởng để đổi voucher</strong></div>
+                </div>
+            </div>
+        )
+    }
 }
